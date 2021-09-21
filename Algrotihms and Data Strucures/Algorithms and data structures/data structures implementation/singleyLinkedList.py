241247fb21ea44ddcfa28a401b4b7b3da5b4ab21
Ahmed Emad Atya <90578831+Copticoder@users.noreply.github.com>
2021-09-21 23:18:14 +0200

    Update singleyLinkedList.py

--- Algrotihms and Data Strucures/Algorithms and data structures/data structures implementation/singleyLinkedList.py
+++ Algrotihms and Data Strucures/Algorithms and data structures/data structures implementation/singleyLinkedList.py
@@ -1,13 +1,3 @@
-"""The LinkedList code from before is provided below.
-Add three functions to the LinkedList.
-"get_position" returns the element at a certain position.
-The "insert" function will add an element to a particular
-spot in the list.
-"delete" will delete the first element with that
-particular value.
-Then, use "Test Run" and "Submit" to run the test cases
-at the bottom."""
-
 class Element(object):
     def __init__(self, value):
         self.value = value
@@ -35,9 +25,7 @@ class LinkedList(object):
                 counter+=1
             
             return current.value 
-        """Get an element from a particular position.
-        Assume the first position is "1".
-        Return "None" if position is not in the list."""
+
     
     def insert(self, new_element, position):
         current=self.head
@@ -50,11 +38,7 @@ class LinkedList(object):
                 
                     current=current.next
                     counter+=1
-        """Insert a new node at the given position.
-        Assume the first position is "1".
-        Inserting at position 3 means between
-        the 2nd and 3rd elements."""
-        pass
+
     
     
     def delete(self, value):
@@ -67,39 +51,32 @@ class LinkedList(object):
                     current=current.next
                 current.next=current.next.next
 
-        """Delete the first node with a given value."""
-        pass
 
-# Test cases
-# Set up some Elements
-# Test cases
-# Set up some Elements
+
+
 e1 = Element(1)
 e2 = Element(2)
 e3 = Element(3)
 e4 = Element(4)
 
-# Start setting up a LinkedList
 ll = LinkedList(e1)
 ll.append(e2)
 ll.append(e3)
 
-# Test get_position
-# Should print 3
+
 print (ll.head.next.next.value)
-# Should also print 3
+
 print (ll.get_position(3))
 
-# Test insert
+
 ll.insert(e4,3)
-# Should print 4 now
+
 print (ll.get_position(3))
 
-# Test delete
 ll.delete(1)
-# Should print 2 now
+
 print (ll.get_position(1))
-# Should print 4 now
+
 print (ll.get_position(2))
-# Should print 3 now
+
 print (ll.get_position(3))
