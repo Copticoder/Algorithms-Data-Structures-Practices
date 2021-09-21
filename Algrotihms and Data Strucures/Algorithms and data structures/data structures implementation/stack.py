18930a6809cfae8f7789af2971796e2456f88dc1
Ahmed Emad Atya <90578831+Copticoder@users.noreply.github.com>
2021-09-21 23:18:53 +0200

    Update stack.py

--- Algrotihms and Data Strucures/Algorithms and data structures/data structures implementation/stack.py
+++ Algrotihms and Data Strucures/Algorithms and data structures/data structures implementation/stack.py
@@ -1,10 +1,3 @@
-"""Add a couple methods to our LinkedList class,
-and use that to implement a Stack.
-You have 4 functions below to fill in:
-insert_first, delete_first, push, and pop.
-Think about this while you're implementing:
-why is it easier to add an "insert_first"
-function than just use "append"?"""
 
 class Element(object):
     def __init__(self, value):
@@ -27,8 +20,7 @@ class LinkedList(object):
     def insert_first(self, new_element):
         new_element.next=self.head
         self.head=new_element
-        "Insert new element as the head of the LinkedList"
-        pass
+     
 
     def delete_first(self):
         if self.head:
@@ -43,8 +35,7 @@ class LinkedList(object):
         else:
             return 'None'
         
-        "Delete the first (head) element in the LinkedList as return it"
-        pass
+      
 
 class Stack(object):
     def __init__(self,top=None):
@@ -53,26 +44,22 @@ class Stack(object):
     def push(self, new_element):
         self.ll.insert_first(new_element)
 
-        "Push (add) a new element onto the top of the stack"
-        pass
+  
 
     def pop(self):
         return self.ll.delete_first()
             
-        "Pop (remove) the first element off the top of the stack and return it"
-        pass
+   
     
-# Test cases
-# Set up some Elements
+
 e1 = Element(1)
 e2 = Element(2)
 e3 = Element(3)
 e4 = Element(4)
 
-# Start setting up a Stack
+
 stack = Stack(e1)
 
-# Test stack functionality
 stack.push(e2)
 stack.push(e3)
 print (stack.pop())
