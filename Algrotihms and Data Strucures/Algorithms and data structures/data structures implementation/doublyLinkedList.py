c578bb53979c53c233eea4898bb0f9b7955c4ae8
Ahmed Emad Atya <90578831+Copticoder@users.noreply.github.com>
2021-09-21 23:16:33 +0200

    Update doublyLinkedList.py

--- Algrotihms and Data Strucures/Algorithms and data structures/data structures implementation/doublyLinkedList.py
+++ Algrotihms and Data Strucures/Algorithms and data structures/data structures implementation/doublyLinkedList.py
@@ -59,39 +59,33 @@ class LinkedList(object):
 
         
 
-# Test cases
-# Set up some Elements
-# Test cases
-# Set up some Elements
+
 e1 = Element(1)
 e2 = Element(2)
 e3 = Element(3)
 e4 = Element(4)
 
-# Start setting up a LinkedList
 ll = LinkedList(e1)
 ll.append(e2)
 ll.append(e3)
 
-# Test get_position
-# Should print 3
+
 print (ll.head.next.value)
 print(ll.head.next.prev.value)
 print(ll.head.next.next.value)
 print(ll.head.next.next.prev.prev.value)
-# # Should also print 3
+
 print (ll.get_position(3).value)
 
-# # Test insert
+
 ll.insert(e4,3)
-# # Should print 4 now
+
 print (ll.get_position(2).value)
 
-# # Test delete
+
 ll.delete(1)
-# # Should print 2 now
+
 print (ll.get_position(1).next.value)
-# # Should print 4 now
+
 print (ll.get_position(2).prev.value)
-# # Should print 3 now
 print (ll.get_position(3).prev.value)
